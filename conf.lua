5689e855f35f4378848abbdd3bd86ed2eeb29a31
Andrew Abbott <legospacy@gmail.com>
2014-12-01 15:26:06 +1100

    Change window resolution to 600x600
    This matches the original version of the game I made.

--- conf.lua
+++ conf.lua
@@ -5,7 +5,7 @@ function love.conf(t)
 
 	t.window.title = "Hue Shift"
 	t.window.icon = nil
-	t.window.width = 800
+	t.window.width = 600
 	t.window.height = 600
 	t.window.borderless = false
 	t.window.resizable = false
