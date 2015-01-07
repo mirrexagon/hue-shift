4c08d1659a15b5b5c40de1ec91fde7ab6ff28818
Andrew Abbott <legospacy@gmail.com>
2015-01-07 12:37:22 +1100

    Make menu row drawing relative
    In other words, row draw functions don't have to translate the drawing
    to the correct row position - the draw origin is translated before the
    function is called, moving the draw origin to the top left of the row.

--- states/menu.lua
+++ states/menu.lua
@@ -63,28 +63,11 @@ local function get_row_pix_y(row_slot)
 	return (row_slot - 1) * ROW_PIX_H + (row_slot) * ROW_PIX_PAD
 end
 
-local function draw_row_rect(row_slot, row_h, alpha)
-	love.graphics.setColor(255, 255, 255, ROW_ALPHA * (alpha or 1))
-	love.graphics.rectangle("fill", ROW_PIX_PAD, get_row_pix_y(row_slot), ROW_PIX_W, get_row_pix_h(row_h))
-end
-
-local function draw_row_name(row_name, row_slot, row_h, alpha)
-	love.graphics.setColor(255, 255, 255, ROW_ALPHA * (alpha or 1))
-	love.graphics.setFont(font_row_title)
-
-	love.graphics.print(
-		row_name,
-		((ROW_PIX_W + ROW_PIX_PAD)/2) - (font_row_title:getWidth(row_name)/2),
-		get_row_pix_y(row_slot) + (get_row_pix_h(row_h)/2) - (font_row_title:getHeight()/2)
-	)
-end
-
-
 local rows = {
 	["BLOCKS"] = {
-		draw = function(self, row_pix_y, row_pix_h, alpha)
-			local block_xdiff = ROW_PIX_W/4
-			local block_y = row_pix_y + ((row_pix_h)/2 - DEFAULT_TILE_LENGTH/2)
+		draw = function(self, row_pix_w, row_pix_h, alpha)
+			local block_xdiff = row_pix_w/4
+			local block_y = (row_pix_h/2) - (DEFAULT_TILE_LENGTH/2)
 
 			for i = 1, 3 do
 				local lalpha = (i <= n_player_blocks and 255 or 64) * alpha
@@ -95,11 +78,14 @@ local rows = {
 					BLOCK_COLORS[i][3],
 					lalpha
 				)
-				love.graphics.rectangle("fill", block_xdiff * i, block_y,
+
+				local x = (block_xdiff * i) - (DEFAULT_TILE_LENGTH/2)
+
+				love.graphics.rectangle("fill", x, block_y,
 					DEFAULT_TILE_LENGTH, DEFAULT_TILE_LENGTH)
 
 				love.graphics.setColor(255, 255, 255, lalpha)
-				love.graphics.draw(img_arrow, block_xdiff * i, block_y)
+				love.graphics.draw(img_arrow, x, block_y)
 			end
 		end,
 
@@ -155,29 +141,43 @@ local function get_total_row_height(up_to_row_n)
 	return total
 end
 
-local function draw_row(row_name, row_slot, row, alpha)
-	alpha = ((row_order[selected_row] == row_name) and 1 or 0.5) * alpha
+local function draw_row_rect(row_h, alpha)
+	love.graphics.setColor(255, 255, 255, ROW_ALPHA * (alpha or 1))
+	love.graphics.rectangle("fill", 0, 0, ROW_PIX_W, get_row_pix_h(row_h))
+end
 
-	local row_h
-	if row then
-		if row.height then
-			row_h = row.height
-		else
-			row_h = 1
-		end
-	else
-		row_h = 1
-	end
+local function draw_row_name(row_name, row_pix_w, row_pix_h, alpha)
+	love.graphics.setColor(255, 255, 255, ROW_ALPHA * (alpha or 1))
+	love.graphics.setFont(font_row_title)
+
+	love.graphics.print(
+		row_name,
+		(row_pix_w/2) - (font_row_title:getWidth(row_name)/2),
+		(row_pix_h/2) - (font_row_title:getHeight()/2)
+	)
+end
+
+local function draw_row(row_n, row_slot, alpha)
+	alpha = ((row_n == selected_row) and 1 or 0.5) * alpha
+
+	local row_h = get_row_height(row_n)
 
 	---
 
-	draw_row_rect(row_slot, row_h, alpha)
+	local row = get_row(row_n)
 
-	draw_row_name(row_name, row_slot, row_h, alpha)
+
+	love.graphics.push()
+	love.graphics.translate(ROW_PIX_PAD, get_row_pix_y(row_slot))
+
+	draw_row_rect(row_h, alpha)
+	draw_row_name(row_order[row_n], ROW_PIX_W, get_row_pix_h(row_h), alpha)
 
 	if row and row.draw then
-		row:draw(get_row_pix_y(row_slot), get_row_pix_h(row_h), alpha)
+		row:draw(ROW_PIX_W, get_row_pix_h(row_h), alpha)
 	end
+
+	love.graphics.pop()
 end
 
 ---
@@ -236,12 +236,10 @@ function menu:draw()
 	love.graphics.translate(0, -math.floor(scroll_offset * (ROW_PIX_H + ROW_PIX_PAD)))
 
 	local offset = 1
-	for i, row_name in ipairs(row_order) do
-		local row = rows[row_name]
+	for row_n, row_name in ipairs(row_order) do
+		draw_row(row_n, offset, global_alpha)
 
-		draw_row(row_name, offset, row, global_alpha)
-
-		offset = offset + get_row_height(i)
+		offset = offset + get_row_height(row_n)
 	end
 
 	love.graphics.pop()
