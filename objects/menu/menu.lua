c1f4015267ffb8c9d7d968983d40e586d475b9f0
Andrew Abbott <legospacy@gmail.com>
2015-10-22 14:22:07 +1100

    Start adding menu

---
+++ objects/menu/menu.lua
@@ -0,0 +1,45 @@
+--- Require ---
+local Class = require("lib.hump.class")
+local timer = require("lib.hump.timer")
+
+local util = require("lib.self.util")
+--- ==== ---
+
+
+--- Localised functions ---
+local floor = math.floor
+--- ==== ---
+
+
+--- Classes ---
+--- ==== ---
+
+
+--- Class definition ---
+local Menu = {}
+--- ==== ---
+
+
+--- Class functions ---
+function Menu:init()
+
+end
+
+
+function Menu:update(dt)
+
+end
+
+
+function Menu:draw()
+
+end
+
+
+function Menu:keypressed(k)
+
+end
+--- ==== ---
+
+
+return Class(Game)
