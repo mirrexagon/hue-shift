c7ceb668f09b91a67b9bc4b7a2dafec4494689db
Andrew Abbott <legospacy@gmail.com>
2015-12-06 14:19:17 +1100

    Clean up main.lua a bit

--- main.lua
+++ main.lua
@@ -4,7 +4,8 @@
 	---
 
 	Music:
-		laserwash by coda (http://coda.s3m.us)
+		Laserwash by coda (http://coda.s3m.us)
+		Hue Shift by coda (http://coda.s3m.us)
 ]]
 
 --[[
@@ -22,14 +23,11 @@
 		Changing theme makes background and block colors on menu fade to new theme
 
 	---
-	---
 
 	BUGS:
 		On song loop, game is off by half a beat. This continues through all subsequent loops.
 
 	TODO:
-		Show both blocks or indicate when two blocks overlap.
-
 		Record scores
 		Obstacle AI
 
@@ -40,34 +38,38 @@
 				+ Faster
 				+ Changes randomly
 
-		Effects:
-			Screen shake - a shake happens every beat, specify intensity level per song?
-				Pattern specified by song?
-			Background flashing - also every beat?
-			Grid scaling - as above
+	---
 
-		Achievements?
-		More backgrounds? Set by music?
+	Effects:
+		Screen shake - a shake happens every beat, specify intensity level per song?
+			Pattern specified by song?
+		Background flashing - also every beat?
+		Grid scaling - as above
 ]]
 
+
+--- Require ---
 local gamestate = require("lib.hump.gamestate")
 local timer = require("lib.hump.timer")
 
 local util = require("lib.self.util")
+--- ==== ---
 
----
 
+--- Gamestates ---
 --local state_menu = require("states.menu")
 local state_game = require("states.game")
+--- ==== ---
 
----
 
+--- Main love functions ---
 function love.load()
 	gamestate.registerEvents()
 	gamestate.switch(state_game)
 end
 
 function love.update(dt)
-	--- Update global timer.
+	-- Update global timer.
 	timer.update(dt)
 end
+--- ==== ---
